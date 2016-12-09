64b43940aefe8062c8152eb49318706cf32eeb36
andreacanale <andreacanale92@gmail.com>
2016-12-09 15:02:58 +0100

    ADDED UI_face

---
+++ src/ui_V2/script.js
@@ -0,0 +1,15 @@
+//top:10% guarda in alto top:90% guarda in basso
+//left:-80% sx 80% a dx
+
+
+
+window.onload=function(){
+
+    $("#TestoProva").text("fanculo2");
+}
+
+
+
+function bella(){
+ $("#TestoProva").text("bellazio");
+}
