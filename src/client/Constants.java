40d8c085c23eee459de237718e1936834eab9fe2
edoardesd <edoardesd@gmail.com>
2017-01-12 14:55:04 +0100

    Aggiornate chiavi bluemix

--- src/client/Constants.java
+++ src/client/Constants.java
@@ -3,14 +3,14 @@ package client;
 public class Constants {
 	public static final String GOOGLE_API_KEY = "AIzaSyCu8HOd6-u_kdINc5-8RgW37ZqN5TvMHks"; 
 	
-	public static final String WATSON_CONVERSATION_USER = "f4117225-c6ba-4b8c-8e52-8a9612c19c97";
-	public static final String WATSON_CONVERSATION_PSW = "P72KZKNXHTce";
-	public static final String WATSON_WORKSPACE_ID = "51b861e1-efdb-40a1-b9ca-19e44c83e4e7";
+	public static final String WATSON_CONVERSATION_USER = "608ff133-e623-472b-8159-e956762f3180";
+	public static final String WATSON_CONVERSATION_PSW = "2OBPZEEU2VEn";
+	public static final String WATSON_WORKSPACE_ID = "d2754181-9b82-4146-bdb6-8ef2223191e9";
 	
-	public static final String WATSON_TRANSLATOR_USER = "fd9544c7-c167-425e-89c0-994722cd51c9";
-	public static final String WATSON_TRANSLATOR_PSW = "qCNmKuevukSz";
+	public static final String WATSON_TRANSLATOR_USER = "33279f60-ca39-4793-a7b6-1085f6097165";
+	public static final String WATSON_TRANSLATOR_PSW = "HwKNbsM3Blzp";
 	
-	public static final String WATSON_ALCHEMY_KEY = "eeff34668d75b2dd9bd200c52242798a59847249";
+	public static final String WATSON_ALCHEMY_KEY = "f164ca0afb7ede22078c41d9c001b5ffce7b9732";
 	
 	public static final String SERVER_URL = "192.168.1.7";
 	public static final int SERVER_PORT= 8189;
