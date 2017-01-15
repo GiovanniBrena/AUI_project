f48511f0344b5ce6caae2bf85410c7fba7469c55
edoardesd <edoardesd@gmail.com>
2017-01-15 20:59:48 +0100

    Aggiornati campi per nuovo database

--- src/database/ConstantsDB.java
+++ src/database/ConstantsDB.java
@@ -3,9 +3,9 @@ package database;
 public class ConstantsDB {
 
 	public static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String DATABASE_URL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_85f9c5363d162e8";
-	public static final String DATABASE_USER = "b0179319c7b2ef";
-	public static final String DATABASE_PSW = "d319ebe7";
+	public static final String DATABASE_URL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_eee466bfc656e61";
+	public static final String DATABASE_USER = "b48fd20759af3f";
+	public static final String DATABASE_PSW = "0729b97e";
 	public static final String NEW_CONVERSATION= "***INIZIO NUOVA CONVERSAZIONE***";
 	public static final String DB_USER = "Database";
 }
